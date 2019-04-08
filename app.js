d9d520d2134fe06ec1cba466e27615e8c0feb99c
Brian Lee <ibltsandwich@gmail.com>
2019-04-08 16:31:27 -0400

    Add initial routes

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const events = require("./routes/api/events");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(db)
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+app.use("/api/users", users);
+app.use("/api/events", events);
 
 app.get("/", (req, res) => res.send("PubStats"));
 
