e7ee989efe53c3a9ae9c9ff10ac1325b3809fb0a
Brian Lee <ibltsandwich@gmail.com>
2019-04-08 16:25:22 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("PubStats"));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
