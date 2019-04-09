c69bffcdc0810d0139253f59feb8e63ab7aac3c6
Brian Lee <ibltsandwich@gmail.com>
2019-04-09 16:04:06 -0400

    Add passport validation

--- config/passport.js
+++ config/passport.js
@@ -8,3 +8,15 @@ const options = {};
 options.jwtFromRequest= ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey= keys.secretOrKey;
 
+module.exports = passport => {
+  passport.use(new JwtStrategy(options, (payload, done) => {
+    User.findById(jwt_payload.id)
+      .then(user => {
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, false);
+      })
+      .catch(err => console.log(err));
+  }));
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const jsonwebtoken = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const User = require('../../models/User');
 const keys = require('../../config/keys');
@@ -83,6 +84,14 @@ router.post('/login', (req, res) => {
           }
         })
     })
+})
+
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  });
 })
 
 module.exports = router;
