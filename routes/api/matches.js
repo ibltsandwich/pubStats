12401278d4de29feb32a0c47b72a33e90b6260a6
Brian Lee <ibltsandwich@gmail.com>
2019-04-15 19:06:41 -0400

    Add get route for matches

--- routes/api/matches.js
+++ routes/api/matches.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const Match = require('../../models/Match');
 
 router.get(`/matches/${matchId}`, (req, res) => {
-
-})
+  Match.findOne({ shardId: req.body.matchId })
+    .then(match => {
+      return res.json({
+        matchId: match
+      });
+    });
+});
