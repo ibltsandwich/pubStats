8ac0be8c2e48c1e2131ec145d5982e66141e33a7
Brian Lee <ibltsandwich@gmail.com>
2019-05-21 18:33:18 -0400

    Add session api util, reducers, and configure index

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import jwt_decode from 'jwt-decode';
+
+import * as APIUtil from './util/session_api_util';
 
 import configureStore from './store/store';
 import Root from './root';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
+
+  if (localStorage.jwtToken) {
+    APIUtil.setAuthToken(localStoage.jwtToken);
+
+    const decoded = jwt_decode(localStorage.jwtToken);
+    store.dispatch(APIUtil.setCurrentUser(decoded));
+
+    const currentTime = Date.now() / 10000;
+    if (decoded.exp < currentTime) {
+      store.dispatch(APIUtil.logoutUser());
+      window.location.href = '/login';
+    }
+  }
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
