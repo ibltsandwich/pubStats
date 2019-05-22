b1794d49f2c6f6ef75b9f86be4930b4af8273703
Brian Lee <ibltsandwich@gmail.com>
2019-05-21 21:50:23 -0400

    Comment out unused file

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const passport = require('passport');
 
 const User = require('../../models/User');
-const keys = require('../../config/keys');
+// const keys = require('../../config/keys');
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
