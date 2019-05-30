ccbc4ac74ae337f4c976d560479e41c7cca598c8
Brian Lee <ibltsandwich@gmail.com>
2019-05-30 16:56:49 -0400

    Obtain match data from api and style player match history

--- frontend/src/components/player/player_stats.jsx
+++ frontend/src/components/player/player_stats.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 
 import { fetchPlayer } from '../../actions/player_actions';
 
+const API = 'https://api.pubg.com/shards/steam/matches/';
+
 const msp = (state, ownProps) => {
   const player = state.entities.players[ownProps.match.params.playerName.toLowerCase()];
   return {
@@ -19,18 +21,65 @@ const mdp = dispatch => {
 class PlayerStats extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {};
   }
 
   componentDidMount() {
     this.props.fetchPlayer(this.props.match.params.playerName.toLowerCase());
   }
 
+  componentDidUpdate(oldProps) {
+    if (Object.values(this.state).length === 0) {
+      this.props.player.matches.data.forEach(match => {
+        fetch(API + match.id , {
+                method: 'GET',
+                headers: {
+                  'Accept': 'application/json'
+                },
+          })
+          .then(response => response.json())
+          // .then(data => this.setState({ [match.id]: data }))
+          .then(matchData => {
+            const matchInfo = {};
+            matchInfo.id = matchData.data.id;
+            matchInfo.attributes = matchData.data.attributes;
+
+            for (let i = 0; i < matchData.included.length; i += 1) {
+              const item = matchData.included[i];
+              if (item.type === "participant") {
+                if (item.attributes.stats.playerId === this.props.player.playerId) {
+                  matchInfo.stats = item.attributes.stats;
+                  return this.setState({ [match.id]: matchInfo })
+                }
+              }
+            }
+          })
+      })
+    }
+  }
+
   render() {
     if(this.props.player) {
       const { player } = this.props;
       const updated = new Date(Date.parse(player.time));
-      const matchHistory = player.matches.data.map((match,idx) => {
-        return <li id="player-match" key={idx}>Match: {match.id}</li>
+      const matchHistory = Object.values(this.state).map((match,idx) => {
+        const gameDate = new Date(match.attributes.createdAt).toLocaleString();
+        const survivalMinutes = Math.floor(match.stats.timeSurvived / 60);
+        const survivalSeconds = Math.round(((match.stats.timeSurvived / 60) % 1) * 60);
+        
+        return (
+          <li id="player-match" key={idx}>
+            {/* <h1>Match: {match.id}</h1> */}
+            <div className="match-attributes">
+              <h2>Played: {gameDate}</h2>
+              <h2>Game Mode: {match.attributes.gameMode.toUpperCase()}</h2>
+            </div>
+            <h3>Time Survived: {survivalMinutes + ":" + survivalSeconds}</h3>
+            <span>Damage Dealt: {match.stats.damageDealt.toFixed(2)}</span>
+            <span>Kills: {match.stats.kills}</span>
+          </li>
+        )
       });
 
       return(
@@ -42,7 +91,7 @@ class PlayerStats extends React.Component {
 
           </header>
           <main className="player-match-history">
-            <ul className="match-history-list">
+            <ul className="player-match-history-list">
               {matchHistory}
             </ul>
           </main>
