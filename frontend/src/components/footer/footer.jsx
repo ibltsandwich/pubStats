7efe3162a6b47283f6c00a9e4f96d9d4b3173b1f
Brian Lee <ibltsandwich@gmail.com>
2019-06-23 23:23:02 -0400

    Add favicon? and footer component

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = props => {
+
+  return (
+    <div className="footer-container">
+      
+    </div>
+  )
+}
+
+export default connect(msp, mdp)(Footer);
