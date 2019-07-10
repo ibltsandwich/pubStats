77f8e25d67d0ee12bac74ccd76ad27ea19eabc4b
Brian Lee <ibltsandwich@gmail.com>
2019-07-10 19:09:48 -0400

    Display teams in ranking order

--- frontend/src/components/match/match_show.jsx
+++ frontend/src/components/match/match_show.jsx
@@ -29,18 +29,79 @@ class MatchShow extends React.Component {
     super(props);
 
     this.state = { loading: true };
-  }
+  };
 
   componentDidMount() {
     this.props.fetchMatch(this.props.matchId);
-  }
+  };
 
   componentDidUpdate(oldProps) {
     if ((this.state.loading && this.props.match) ||
       (this.state.loading && this.props.matchErrors["Not Found"])) {
       this.setState({loading: false});
     };
+  };
+
+  parseTeams() {
+    const {rosters, participants} = this.props.match;
+    const matchTeams = {};
+
+    Object.values(rosters).forEach(roster => {
+      const rank = roster.attributes.stats.rank;
+      matchTeams[roster.id]= [rank];
+      
+      roster.relationships.participants.data.forEach(participant => {
+        matchTeams[roster.id].push(participants[participant.id]);
+      });
+    });
+
+    const sortedTeams = Object.values(matchTeams).sort((a,b) => {
+      return a[0] - b[0];
+    });
+    
+    const teamsArray = sortedTeams.map((team,idx) => {
+      const rank = team[0];
+
+      const playersArray = team.map(player => {
+        if (typeof(player) !== "number") {
+          return <h2><Link to={`/players/${player.attributes.stats.name}`}>
+            {player.attributes.stats.name}
+          </Link></h2>;
+        }
+      });
+
+      let className;
+      if (rank == 1) {
+        className = "winner";
+      } else if (rank <= 10) {
+        className = "top-10";
+      } else {
+        className = "dnp";
+      };
+
+      return (<li className="match-show-team" key={idx}>
+        <h1 className={className}>#{rank}</h1>
+        <div className="match-show-team-members">
+          {playersArray}
+        </div>
+      </li>);
+    });
+
+    return teamsArray;
   }
+
+  setMapName(map) {
+    switch(map) {
+      case "Erangel_Main":
+        return "Erangel";
+      case "Savage_Main":
+        return "Sanhok";
+      case "DihorOtok_Main":
+        return "Vikendi";
+      case "Desert_Main":
+        return "Miramar";
+    };
+  };
 
   render() {
     if (!this.state.loading) {
@@ -54,51 +115,17 @@ class MatchShow extends React.Component {
       } else if (this.props.match) {
 
         const {attributes, rosters, participants} = this.props.match;
-        const matchTeams = {};
-
-        Object.values(rosters).forEach(roster => {
-          roster.relationships.participants.data.forEach(participant => {
-            const rank = roster.attributes.stats.rank;
-
-            if (matchTeams[rank]) {
-              matchTeams[rank].push(participants[participant.id]);
-            } else{
-              matchTeams[rank] = [participants[participant.id]];
-            };
-          });
-        });
-
-        let teamsArray = [];
-        for (let key in matchTeams) {
-          const playersArray = matchTeams[key].map(player => {
-            return <h2><Link to={`/players/${player.attributes.stats.name}`}>
-              {player.attributes.stats.name}
-            </Link></h2>;
-          });
-
-          let className;
-          if (key == 1) {
-            className = "winner"
-          } else if (key <= 10) {
-            className = "top-10"
-          } else {
-            className = "dnp"
-          }
-
-          teamsArray.push(<li className="match-show-team" key={key}>
-            <h1 className={className}>#{key}</h1>
-            <div className="match-show-team-members">
-              {playersArray}
-            </div>
-          </li>);
-        }
+        const teamsArray = this.parseTeams()
 
         const gameDate = new Date(attributes.createdAt).toLocaleString();
+        const mapName = this.setMapName(attributes.mapName).toUpperCase();
 
         return (
           <div className="match-show-container">
             <header className="match-show-header">
-              <h1 className="match-show-time">{gameDate}</h1>
+              <h1>{mapName}</h1>
+              <h2>{attributes.gameMode.toUpperCase()}</h2>
+              <h3 className="match-show-time">{gameDate}</h3>
             </header>
             <section>
               <ul>
